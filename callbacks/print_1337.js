a2b5557de4b21e1b56d4e103a7dc7bcc0601e055
Matt Kelly <bmatthewkelly@gmail.com>
2018-07-09 19:25:48 +0000

    created step 6 files, transformed how we printed the html code

---
+++ callbacks/print_1337.js
@@ -0,0 +1,12 @@
+// var getHTML = require('./step5');
+
+// var requestOptions = {
+//   host: 'sytantris.github.io',
+//   path: '/http-examples/step6/1337.html'
+// };
+
+// function printHTML(html) {
+//   console.log(html);
+// }
+
+// getHTML(requestOptions, printHTML);
